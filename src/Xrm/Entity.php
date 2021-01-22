5a56206e261f4a56cfea7759f05d56031cd24572
Pavel Selitskas <p.selitskas@gmail.com>
2021-01-22 16:45:50 +0300

    Allow cloning Entity instances

--- src/Xrm/Entity.php
+++ src/Xrm/Entity.php
@@ -93,7 +93,7 @@ class Entity implements \ArrayAccess {
             return null;
         }
 
-        return $this->Attributes[$attribute];
+        return $this->Attributes[ $attribute ];
     }
 
     /**
@@ -110,7 +110,7 @@ class Entity implements \ArrayAccess {
             return '';
         }
 
-        return $this->FormattedValues[$attribute];
+        return $this->FormattedValues[ $attribute ];
     }
 
     /**
@@ -120,8 +120,8 @@ class Entity implements \ArrayAccess {
      * @param mixed $value
      */
     public function SetAttributeValue( string $attribute, $value ): void {
-        $this->Attributes[$attribute] = $value;
-        $this->attributeState[$attribute] = true;
+        $this->Attributes[ $attribute ] = $value;
+        $this->attributeState[ $attribute ] = true;
     }
 
     /**
@@ -188,7 +188,7 @@ class Entity implements \ArrayAccess {
      *
      * @return void
      */
-    public function offsetUnset($offset): void {
+    public function offsetUnset( $offset ): void {
         unset( $this->Attributes[ $offset ], $this->attributeState[ $offset ] );
     }
 
@@ -196,4 +196,25 @@ class Entity implements \ArrayAccess {
         return $this->attributeState;
     }
 
+    /**
+     * When an object is cloned, PHP 5 will perform a shallow copy of all of the object's properties.
+     * Any properties that are references to other variables, will remain references.
+     * Once the cloning is complete, if a __clone() method is defined,
+     * then the newly created object's __clone() method will be called, to allow any necessary properties that need to be changed.
+     * NOT CALLABLE DIRECTLY.
+     *
+     * @return void
+     * @link https://php.net/manual/en/language.oop5.cloning.php
+     */
+    public function __clone() {
+        $this->attributeState = clone $this->attributeState;
+
+        foreach ( $this->Attributes as $field => &$value ) {
+            if ( !is_object( $value ) ) {
+                continue;
+            }
+
+            $value = clone $value;
+        }
+    }
 }
